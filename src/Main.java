806d6a97d00a42980337ed7841e48ade564acf47
kinomagnum <kinomagnum@gmail.com>
2024-05-27 23:46:49 +0300

    Avia1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int amountiket = 18783; // стоимость билета
+        int rubmill = 20; // Количество рублей за 1 милю
+        int bonusMill = amountiket / rubmill;
+        System.out.println(" За билет стоимостью " + amountiket + " Начислено бонусных миль " + bonusMill);
+    }
+}
+
+
