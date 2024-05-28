9c9b8602cd2f46e1828741e370f505a7110e4e42
kinomagnum <kinomagnum@gmail.com>
2024-05-28 12:18:35 +0300

    Aviakamelkase

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int amountiket = 18783; // стоимость билета
-        int rubmill = 20; // Количество рублей за 1 милю
-        int bonusMill = amountiket / rubmill;
-        System.out.println(" За билет стоимостью " + amountiket + " Начислено бонусных миль " + bonusMill);
+        int amountTiket = 18783; // стоимость билета
+        int rubMill = 20; // Количество рублей за 1 милю
+        int bonusMill = amountTiket / rubMill;
+        System.out.println(" За билет стоимостью " + amountTiket + " Начислено бонусных миль " + bonusMill);
     }
 }
 
